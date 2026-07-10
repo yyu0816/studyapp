f01e6fb815c433c177493e4658d8d5fde00ae01f
yyu0816 <tokagetokage1@gmail.com>
2026-07-10 23:34:22 +0800

    fix code

--- studyapp.py
+++ studyapp.py
@@ -407,5 +407,5 @@ def index() -> str:
     return render_home_page()
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     app.run(debug=True, host="0.0.0.0", port=8000)
